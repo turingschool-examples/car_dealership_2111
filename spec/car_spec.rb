6c1a1a67645a0aaef653f44ad488c10873d5a518
Michael Dao <mike@turing.io>
2021-03-30 17:55:55 -0600

    initial commit

---
+++ spec/car_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require './lib/car'
+
+RSpec.describe Car do
+  it 'exists' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car).to be_a Car
+  end
+
+  it 'has a make' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.make).to eq("Ford")
+  end
+
+  it 'has a model' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.model).to eq("Mustang")
+  end
+
+  it 'has a monthly payment' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.monthly_payment).to eq(1500)
+  end
+
+  it 'has a loan length' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.loan_length).to eq(36)
+  end
+
+  it 'has a total cost' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.total_cost).to eq(54000)
+  end
+end
