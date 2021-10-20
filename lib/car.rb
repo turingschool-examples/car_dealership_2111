5115a658ccb28e38615d3f5c349e0447665e5fb8
Kevin Gloss <kgloss89@gmail.com>
2021-10-20 15:36:00 -0600

    Refactor the initialize parameters

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,9 @@
 class Car
   attr_reader :make, :model, :monthly_payment, :loan_length, :color
 
-  def initialize(make, model = nil, monthly_payment, loan_length)
-    @make = make.split[0]
-    @model = make.split[1]
+  def initialize(make_model, monthly_payment, loan_length)
+    @make = make_model.split[0]
+    @model = make_model.split[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
