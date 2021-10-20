24cd5cd8a3cf1c4a679b88567de14c5bafdbd0d8
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-20 15:02:31 -0600

    Add add_car method test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -19,6 +19,20 @@ RSpec.describe Dealership do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
 
     expect(dealership.inventory_count).to eq(0)
+  end
 
+  it 'can add cars to inventory array' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expected_array = [car_1, car_2]
+
+    expect(dealership.inventory).to eq(expected_array)
+    expect(dealership.inventory_count).to eq(2)
   end
 end
