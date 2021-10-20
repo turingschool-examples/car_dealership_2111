72d84742bd8d0fe9d6d3ecd55ebf03e861616e7d
Eldridge Turambi <eturambi95@gmail.com>
2021-10-20 14:31:46 -0600

    Finish Iteration 2

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,4 +1,19 @@
 class Dealership
+  attr_reader :name,
+              :address,
+              :inventory
 
-  def initialize
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
+
+  def add_car(car_instance)
+    @inventory << car_instance
+  end
+
+  def inventory_count
+    @inventory.length
+  end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe Dealership do
   it 'exists' do
     expect(@dealership).to be_an_instance_of(Dealership)
   end
+
+  it 'has attributes' do
+    expect(@dealership.name).to eq('Acme Auto')
+    expect(@dealership.address).to eq('123 Main Street')
+  end
+
+  it 'has empty inventory by default' do
+    expect(@dealership.inventory).to eq([])
+  end
+
+  it '#add_car' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    expect(@dealership.inventory).to eq([@car_1, @car_2])
+  end
+
+  it '#inventory_count' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    expect(@dealership.inventory_count).to eq(2)
+  end
 end
