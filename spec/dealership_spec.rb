cf6ebb26d14555ff414020661d16f4cecfa64f23
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-20 14:35:21 -0600

    Test: Add cars_by_make test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -5,6 +5,8 @@ describe Dealership do
   before(:each) do
     @car_1 = Car.new("Ford Mustang", 1500, 36)
     @car_2 = Car.new("Toyota Prius", 1000, 48)
+    @car_3 = Car.new("Toyota Tercel", 500, 48)
+    @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
   end
 
   it 'exists' do
@@ -49,6 +51,18 @@ describe Dealership do
       expect(dealership.has_inventory?).to eq(true)
       dealership.add_car(@car_2)
       expect(dealership.has_inventory?).to eq(true)
+    end
+  end
+
+  describe ' #cars_by_make' do
+    it 'returns only cars of specified make from inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      dealership.add_car(@car_1)
+      dealership.add_car(@car_2)
+      dealership.add_car(@car_3)
+      dealership.add_car(@car_4)
+      expect(dealership.cars_by_make("Toyota")).to eq([@car_2, @car_3])
+      expect(dealership.cars_by_make("Ford")).to eq([@car_1])
     end
   end
 
