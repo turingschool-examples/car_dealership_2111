64573f3153504c7159076e768a18af217765e871
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-20 14:16:32 -0600

    Pass total_cost test

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe Car do
     expect(car.loan_length).to eq(36)
   end
 
-  xit 'has a total cost' do
+  it 'has a total cost' do
     car = Car.new("Ford Mustang", 1500, 36)
 
     expect(car.total_cost).to eq(54000)
