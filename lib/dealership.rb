b0a15f7b3c4b4ed84e9d87edb4cb200f0c45c876
Brad Breiten <jonathan@breiten.us>
2021-10-20 14:57:25 -0600

    Complete iteration 3 methods and tests

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -12,8 +12,37 @@ class Dealership
     @inventory.length
   end
 
+  def has_inventory?
+    @inventory.length >= 1
+  end
+
   def add_car(car)
     @inventory << car
+  end
+
+  def cars_by_make(make)
+    cars_by_make_catcher = []
+    @inventory.each do |car|
+      if car.make == make
+        cars_by_make_catcher << car
+      end
+    end
+    cars_by_make_catcher
+  end
+
+  def total_value
+    total_inventory_value = 0
+    @inventory.each do |car|
+      total_inventory_value += car.total_cost
+    end
+    total_inventory_value
+  end
+
+  def details
+    dealer_details = {
+      "total_value" => total_value,
+      "address" => @address
+    }
   end
 
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -31,6 +31,14 @@ RSpec.describe Dealership do
     end
   end
 
+  describe '#has_inventory' do
+    it 'tells if there is no inventory' do
+      expect(@dealership.inventory.length).to eq 0
+      expect(@dealership.has_inventory?).to eq false
+    end
+  end
+
+
   describe '#add_car' do
     it 'adds car to inventory' do
       car_1 = Car.new("Ford Mustang", 1500, 36)
@@ -40,6 +48,40 @@ RSpec.describe Dealership do
 
       expect(@dealership.inventory).to eq([car_1, car_2])
       expect(@dealership.inventory_count).to eq 2
+      expect(@dealership.has_inventory?).to eq true
+    end
+  end
+end
+
+RSpec.describe Dealership do
+  before(:each) do
+    @dealership = Dealership.new("Acme Auto", "123 Main Street")
+    @car_1 = Car.new("Ford Mustang", 1500, 36)
+    @car_2 = Car.new("Toyota Prius", 1000, 48)
+    @car_3 = Car.new("Toyota Tercel", 500, 48)
+    @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+  end
+
+  describe '#cars_by_make' do
+    it 'returns array of cars with chosen make' do
+      expect(@dealership.cars_by_make("Toyota")).to eq([@car_2, @car_3])
+      expect(@dealership.cars_by_make("Ford")).to eq([@car_1])
+    end
+  end
+
+  describe '#total_value' do
+    it 'returns total value of month cost * loan for all cars' do
+      expect(@dealership.total_value).to eq 156000
+    end
+  end
+
+  describe '#dealer_details' do
+    it 'returns valuea nd address of dealership in hash' do
+      expect(@dealership.details).to eq ({"total_value" => 156000, "address" => "123 Main Street"})
     end
   end
 end
