c69f0b65aff775d935f5e7dd954f7c1015a07450
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-20 14:37:19 -0600

    Add inventory/inventory_size tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -5,9 +5,20 @@ require './lib/dealership'
 RSpec.describe Dealership do
 
   it 'is an instance of Dealership' do
-
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
 
   end
 
+  it 'has an empty inventory on creation' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+  end
+
+  it 'can read inventory size' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+
+  end
 end
