fc371fc3a4a27688eaec1d7609193cbc4b8292a7
Joel Grant <joelmgrant@me.com>
2021-10-20 15:44:35 -0600

    Test: Add #average_price_of_car tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -81,5 +81,20 @@ describe Dealership do
     end
   end
 
+  describe '#average_price_of_car' do
+    it 'returns the average price of all cars in inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.average_price_of_car).to eq("39,000")
+    end
+  end
+
 
 end
