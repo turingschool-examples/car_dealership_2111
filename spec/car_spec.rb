6c145d1839ca2103e96ad2133691079ef681f7d9
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-20 18:01:54 -0400

    Add tests

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -37,4 +37,10 @@ RSpec.describe Car do
 
     expect(car.total_cost).to eq(54000)
   end
+
+  it 'has a color' do
+    car = Car.new("Ford Mustang", 1500, 36)
+    car.paint!(:blue)
+    expect(car.color).to eq(:blue)
+  end
 end
