74ddbb1257613ea6029bbd5c227352d38a137c0e
Joel Grant <joelmgrant@me.com>
2021-10-20 14:56:08 -0600

    Corrected #inventory_count functionality

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -5,7 +5,15 @@ class Dealership
     @dealership = dealership
     @address = address
     @inventory = []
-    @inventory_count = @inventory.size
+    # @inventory_count = @inventory.size # why can't I do this?
+  end
+
+  def add_car(car)
+    @inventory.push(car)
+  end
+
+  def inventory_count
+    @inventory.size
   end
 
 
