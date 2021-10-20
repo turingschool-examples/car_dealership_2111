1f971518c6b3d7bcd053b7cc420994534953f4b9
Joel Grant <joelmgrant@me.com>
2021-10-20 15:00:29 -0600

    Test: Add #cars_by_make tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -13,7 +13,7 @@ describe Dealership do
   describe '#inventory' do
     it 'returns the inventory of the dealership' do
       dealership = Dealership.new("Acme Auto", "123 Main Street")
-      expect(dealership.intentory).to eq([])
+      expect(dealership.inventory).to eq([])
     end
   end
 
@@ -34,7 +34,21 @@ describe Dealership do
       expect(dealership.inventory).to eq([car_1, car_2]) # also re-tests #inventory
       expect(dealership.inventory_count).to eq(2)
     end
+  end
 
+  describe '#cars_by_make()' do
+    it 'returns a list of cars based on make' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.cars_by_make("Toyota")).to eq ([car_2, car_3])
+    end
   end
 
 
