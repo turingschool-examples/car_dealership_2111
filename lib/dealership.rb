9c482386e1f6a074496c1168554a5e5e2ed1dc58
David Kassin <david.j.kassin@gmail.com>
2021-10-20 15:59:52 -0600

    Final commit

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -41,4 +41,22 @@ class Dealership
     details.store("address", address)
     details
   end
+
+
+  def average_price_of_car
+  avg_car = []
+    @inventory.each do |car|
+      avg_car << car.total_cost
+    end
+  avg_car
+  car_sum = 0
+    avg_car.each do |cost|
+      car_sum += cost
+    end
+  avg = car_sum / avg_car.count
+  # Could not find an easy way that I understood to add commas.
+  ## I know I should be using a helper method to calculate the average
+  ## but I was pressed for times.
+  (avg).to_s
+  end
 end
