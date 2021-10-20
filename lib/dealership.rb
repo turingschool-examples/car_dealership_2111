a11d4eaef21df34815ddbe2a75a25a4228423e40
Eldridge Turambi <eturambi95@gmail.com>
2021-10-20 14:50:14 -0600

    Finish Iteration 3

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -16,4 +16,24 @@ class Dealership
   def inventory_count
     @inventory.length
   end
+
+  def cars_by_make(make_of_car)
+    @inventory.find_all do |car|
+      car.make == make_of_car
+    end
+  end
+
+  def total_value
+    @inventory.sum do |car|
+      car.total_cost
+    end
+  end
+
+  def details
+    details_hash ={
+      "total_value" => total_value,
+      "address" => @address
+    }
+    details_hash
+  end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Dealership do
     @dealership = Dealership.new("Acme Auto", "123 Main Street")
     @car_1 = Car.new("Ford Mustang", 1500, 36)
     @car_2 = Car.new("Toyota Prius", 1000, 48)
+    @car_3 = Car.new("Toyota Tercel", 500, 48)
+    @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
   end
 
   it 'exists' do
@@ -31,5 +33,33 @@ RSpec.describe Dealership do
     @dealership.add_car(@car_1)
     @dealership.add_car(@car_2)
     expect(@dealership.inventory_count).to eq(2)
+  end
+
+  it '#cars_by_make' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+
+    expect(@dealership.cars_by_make('Toyota')).to eq([@car_2, @car_3])
+    expect(@dealership.cars_by_make('Ford')).to eq([@car_1])
+  end
+
+  it '#total_value' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+
+    expect(@dealership.total_value).to eq(156000)
+  end
+
+  it '#details' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+
+    expect(@dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
   end
 end
