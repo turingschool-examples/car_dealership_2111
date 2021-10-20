81efd9fbc0dd587f4f393146f920ff8e4b0f35f0
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-20 14:25:13 -0600

    Add paint! method test

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -43,4 +43,12 @@ RSpec.describe Car do
 
     expect(car.color).to eq(nil)
   end
+
+  it 'can change car color' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    car.paint!(:blue)
+
+    expect(car.color).to eq(:blue)
+  end
 end
