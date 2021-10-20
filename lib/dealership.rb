f4aa71e839a9d5c618a66efae4adef13f1b5ab0e
Joel Grant <joelmgrant@me.com>
2021-10-20 15:13:05 -0600

    Add cars_by_make feature

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -16,6 +16,13 @@ class Dealership
     @inventory.size
   end
 
-
+  def cars_by_make(make) # look at how to refactor this without the if statement
+    car_match = []
+    @inventory.find_all do |car|
+      if car.make == make
+        car_match.push(car)
+      end
+    end
+  end
 
 end
