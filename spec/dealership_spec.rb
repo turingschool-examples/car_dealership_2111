af39e88b5fcfec9c4dc08b7e9300e0fb588a9455
Joel Grant <joelmgrant@me.com>
2021-10-20 15:06:19 -0600

    Test: Add #details tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -66,5 +66,20 @@ describe Dealership do
     end
   end
 
+  describe '#details' do
+    it 'returns the basic information of the dealership' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
+    end
+  end
+
 
 end
