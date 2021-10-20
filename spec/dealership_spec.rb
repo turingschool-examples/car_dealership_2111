fda69b4f3bc54e71505994300c29d8da96eafeb9
David Kassin <david.j.kassin@gmail.com>
2021-10-20 14:39:50 -0600

    test: can show inventory and inventory count

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -7,4 +7,14 @@ describe Dealership do
      dealership = Dealership.new("Acme Auto", "123 Main Street")
      expect(dealership).to be_a Dealership
    end
+
+   it 'shows class inventory' do
+     dealership = Dealership.new("Acme Auto", "123 Main Street")
+        expect(dealership.inventory).to eq []
+   end
+
+   it 'shows class inventory count' do
+     dealership = Dealership.new("Acme Auto", "123 Main Street")
+     expect(dealership.inventory_count).to eq(0)
+   end
 end
