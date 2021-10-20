cfc1267d2dcc72fee2cc9d43de5032d58db2d433
Kevin Gloss <kgloss89@gmail.com>
2021-10-20 15:07:56 -0600

    Add average price of cars method

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -5,7 +5,7 @@ class Dealership
     @name = name
     @address = address
     @inventory = []
-
+    @inventory_value = 0
   end
 
   def has_inventory?
@@ -18,9 +18,14 @@ class Dealership
 
   def add_car(new_car)
     @inventory << new_car
+    @inventory_value += new_car.total_cost
   end
 
   def cars_by_make(make)
     sorted_cars = inventory.select {|car| car.make == make}
+  end
+
+  def average_price_of_car
+    (@inventory_value/inventory_count).to_s.insert(2, ',')
   end
 end
