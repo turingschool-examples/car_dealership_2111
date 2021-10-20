2c3d370d9944c7645f0d094794c6eac7e5d0cdae
Joel Grant <joelmgrant@me.com>
2021-10-20 14:51:57 -0600

    Create inventory_count feature

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,11 +1,13 @@
 class Dealership
-  attr_accessor :inventory
+  attr_accessor :inventory, :inventory_count
 
   def initialize(dealership, address)
     @dealership = dealership
     @address = address
     @inventory = []
+    @inventory_count = @inventory.size
   end
+
 
 
 end
