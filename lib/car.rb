ccbd1035299b6e18f33ae51f25354a8fe6fe7ce3
Joel Grant <joelmgrant@me.com>
2021-10-20 14:12:12 -0600

    Create #make method

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,15 @@
 class Car
   attr_accessor :make_model, :monthly_payment, :loan_length
-  
+
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = make_model
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
 
+  def make
+    make = []
+    make = @make_model.split(" ")
+    make[0]
+  end
 end
