657eeedffaa782c38ac3e1c7d9d0697b4ff25eaa
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-20 15:16:05 -0600

    Add cars_by_make method test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -48,6 +48,26 @@ RSpec.describe Dealership do
     dealership.add_car(car_1)
 
     expect(dealership.has_inventory?).to eq true
+  end
+
+  it 'can select cars of a given make' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expected_array = [car_2, car_3]
+
+
+
+    expect(dealership.cars_by_make("Toyota")).to eq(expected_array)
+
 
   end
 end
