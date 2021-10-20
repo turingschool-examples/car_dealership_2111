264b202071ba062c50220a936b3952469f64db72
Brad Breiten <jonathan@breiten.us>
2021-10-20 14:16:22 -0600

    Feat: add car class and test

---
+++ lib/car.rb
@@ -0,0 +1,23 @@
+class Car
+
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
+
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = make_model
+    @make = make_model.split[0]
+    @model = make_model.split[1]
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+    @color = nil
+  end
+
+  def total_cost
+    @monthly_payment * @loan_length
+  end
+
+  def paint!(color)
+    @color = color
+  end
+
+
+end
