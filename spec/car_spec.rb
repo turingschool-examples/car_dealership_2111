98a722b1ceaf5cf7b1600a47d59bb3dfadc02d89
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-10-20 15:31:10 -0600

    Test: created tests for Car class

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,39 +2,47 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
-  it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
+  let(:car) {Car.new("Ford Mustang", 1500, 36)}
 
-    expect(car).to be_a Car
+  # Test initialization of the Car class
+  describe '#initialize' do
+     it 'exists' do
+      expect(car).to be_a Car
+    end
+
+    it 'has a make' do
+      expect(car.make).to eq("Ford")
+    end
+
+    it 'has a model' do
+      expect(car.model).to eq("Mustang")
+    end
+
+    it 'has a monthly payment' do
+      expect(car.monthly_payment).to eq(1500)
+    end
+
+    it 'has a loan length' do
+      expect(car.loan_length).to eq(36)
+    end
+
+    it 'has a total cost' do
+      expect(car.total_cost).to eq(54000)
+    end
   end
 
-  it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.make).to eq("Ford")
-  end
-
-  it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.model).to eq("Mustang")
-  end
-
-  it 'has a monthly payment' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.monthly_payment).to eq(1500)
-  end
-
-  it 'has a loan length' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.loan_length).to eq(36)
-  end
-
-  it 'has a total cost' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.total_cost).to eq(54000)
+  # Test Car#paint! method. 
+  describe '#paint!' do
+    it 'can change color' do
+      car.paint!(:blue)
+      expect(car.color).to eq(:blue)
+    end
+    
+    it 'can change the color again' do
+      car.paint!(:blue)
+      expect(car.color).to eq(:blue)
+      car.paint!(:green)
+      expect(car.color).to eq(:green)
+    end
   end
 end
