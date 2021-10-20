aabaf398969f821111bf14a57add0cee88a98594
Kevin Gloss <kgloss89@gmail.com>
2021-10-20 15:35:15 -0600

    Add total value reader and details method

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,11 +1,11 @@
 class Dealership
-  attr_reader :name, :address, :inventory
+  attr_reader :name, :address, :inventory, :total_value
 
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
-    @inventory_value = 0
+    @total_value = 0
   end
 
   def has_inventory?
@@ -18,21 +18,30 @@ class Dealership
 
   def add_car(new_car)
     @inventory << new_car
-    @inventory_value += new_car.total_cost
+    @total_value += new_car.total_cost
   end
 
   def cars_by_make(make)
     sorted_cars = inventory.select {|car| car.make == make}
   end
 
+  def details
+    dealership = {
+      'total_value' => @total_value,
+      'address' => @address
+    }
+  end
+
+  #instead of setting up a loop to tally this, created a inventory_value counter that tracks total value
   def average_price_of_car
-    (@inventory_value/inventory_count).to_s.insert(2, ',')
+    (@total_value/inventory_count).to_s.insert(2, ',')
   end
 
   def cars_sorted_by_price
-    sorted_cars = @inventory.sort_by {|car| car.total_cost}
+    @inventory.sort_by {|car| car.total_cost}
   end
 
+  #group_by will assign each car object to the car's make condition we set
   def inventory_hash
     inventory.group_by {|car| car.make}
   end
