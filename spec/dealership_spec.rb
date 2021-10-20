9a75634337b656926364974c92815611c2dc6d6a
Parker <parkerlockhart42@gmail.com>
2021-10-20 16:57:24 -0500

    Started on sorted_by_price test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -65,5 +65,31 @@ describe Dealership do
     dealership.add_car(car_4)
     expect(dealership.average_price_of_car).to eq("39,000")
   end
+  # 
+  # it 'sorts cars by price lowest to highest' do
+  #   dealership = Dealership.new("Acme Auto", "123 Main Street")
+  #   car_1 = Car.new("Ford Mustang", 1500, 36)
+  #   car_2 = Car.new("Toyota Prius", 1000, 48)
+  #   car_3 = Car.new("Toyota Tercel", 500, 48)
+  #   car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+  #   dealership.add_car(car_1)
+  #   dealership.add_car(car_2)
+  #   dealership.add_car(car_3)
+  #   dealership.add_car(car_4)
+  #   expect(dealership.cars_sorted_by_price[])
+  # end
+  #
+  # it 'puts inventory into a hash by make' do
+  #   dealership = Dealership.new("Acme Auto", "123 Main Street")
+  #   car_1 = Car.new("Ford Mustang", 1500, 36)
+  #   car_2 = Car.new("Toyota Prius", 1000, 48)
+  #   car_3 = Car.new("Toyota Tercel", 500, 48)
+  #   car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+  #   dealership.add_car(car_1)
+  #   dealership.add_car(car_2)
+  #   dealership.add_car(car_3)
+  #   dealership.add_car(car_4)
+  #   expect(dealership.inventory_hash).to be_a Hash
+  # end
 
 end
