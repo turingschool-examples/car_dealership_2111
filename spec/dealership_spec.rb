8d7ee7daf5e1625829b0a29ddd5bac39d975afc3
David Kassin <david.j.kassin@gmail.com>
2021-10-20 15:11:27 -0600

    test: Show cars by make using cars_by_make method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -26,6 +26,7 @@ describe Dealership do
      dealership.add_car(car_2)
      expect(dealership.inventory).to eq([car_1,car_2])
    end
+
    it '#add_car adds cars to inventory' do
      dealership = Dealership.new("Acme Auto", "123 Main Street")
      car_1 = Car.new("Ford Mustang", 1500, 36)
@@ -37,7 +38,20 @@ describe Dealership do
 
    it 'show has_inventory returns false if empty' do
      dealership = Dealership.new("Acme Auto", "123 Main Street")
-
      expect(dealership.has_inventory?).to eq(false)
+   end
+
+   it '#cars_by_make returns array of cars by make' do
+     dealership = Dealership.new("Acme Auto", "123 Main Street")
+     car_1 = Car.new("Ford Mustang", 1500, 36)
+     car_2 = Car.new("Toyota Prius", 1000, 48)
+     car_3 = Car.new("Toyota Tercel", 500, 48)
+     car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+     dealership.add_car(car_1)
+     dealership.add_car(car_2)
+     dealership.add_car(car_3)
+     dealership.add_car(car_4)
+     expect(dealership.cars_by_make("Toyota")).to eq([car_2,car_3])
+     expect(dealership.cars_by_make("Ford")).to eq([car_1])
    end
 end
