6240a726ba2b463aa6a507cdabcfec2bc5a56240
Chris Hewitt <agop5134@gmail.com>
2021-10-20 14:20:04 -0600

    Iteration 2 dealership and spec

---
+++ lib/dealership.rb
@@ -0,0 +1,18 @@
+class Dealership
+  attr_reader   :name,
+                :address,
+                :inventory,
+                :inventory_count
+  def initialize(name, address)
+    @name            = name
+    @address         = address
+    @inventory       = []
+    @inventory_count = 0
+  end
+
+  def add_car(car)
+    @inventory << car
+    @inventory_count += 1
+  end
+
+end
---
+++ spec/dealership_spec.rb
@@ -0,0 +1,28 @@
+require "./lib/car"
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it "exists" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership).to be_a(Dealership)
+  end
+
+  it "default attributes" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.name).to eq("Acme Auto")
+    expect(dealership.address).to eq("123 Main Street")
+    expect(dealership.inventory).to eq([])
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+  it "add car" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    expect(dealership.inventory).to eq([car_1, car_2])
+    expect(dealership.inventory_count).to eq(2)
+  end
+
+end
