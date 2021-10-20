e70c2f11c6dcfba9f1942d0d29177c8479f93a17
Parker <parkerlockhart42@gmail.com>
2021-10-20 16:26:09 -0500

    Add total value test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -39,4 +39,18 @@ describe Dealership do
     dealership.add_car(car_4)
     expect(dealership.cars_by_make("Toyota").size).to eq(2)
   end
+
+  it 'gives a total of all cars value' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.total_value).to eq(15600)
+  end
+
 end
