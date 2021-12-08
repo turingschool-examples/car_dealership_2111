21949c55d739a5972df64c73736bff6e0d89927f
Tyler Ross <tross020890@gmail.com>
2021-12-08 10:20:47 -0600

    Feat: add make, model

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,17 @@
 class Car
-
+  attr_reader :make, :model, :monthly_payment, :loan_length
   def initialize(name, monthly_payment, loan_length)
     @name = name
     @monthly_payment = monthly_payment
     @loan_length = loan_length
-    @make
-    @model
+
+    split = name.split(' ')
+    @make = split.first
+    @model = split.last
   end
 
-end  
+  def total_cost
+    @monthly_payment * @loan_length
+  end
+
+end
