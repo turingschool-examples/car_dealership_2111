93af4b67a36c86e6dd3f5c81c88d8ec77b765b5b
Michael Brandt <msbrandt00@gmail.com>
2021-12-08 09:18:24 -0700

    Feature: add #inventory_count method

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,7 +4,12 @@ class Dealership
   def initiailize(name, location)
     @name = name
     @location = location
+    @inventory = []
   end
 
-  
+  def inventory_count
+    @inventory.length
+  end 
+
+
 end
