66801a097d3e9f4ca4ae094d6f31b4f616aaeb44
Nick Cooley <cooley8706@gmail.com>
2021-12-08 10:12:45 -0700

    feat: Define paint blue method

--- lib/car.rb
+++ lib/car.rb
@@ -1,23 +1,24 @@
 class Car
   attr_reader :car, :make, :model, :monthly_payment,
-  :loan_length
+  :loan_length, :color
   def initialize(make, model, loan_length)
     @car = car
     @make = "Ford"
     @model = "Mustang"
     @loan_length = 36
     @monthly_payment = 1500
+    @color = nil
   end
 
   def total_cost
     @monthly_payment * @loan_length
   end
 
-  def color
-    @car = nil
+  # def color
+  #   @car = nil
+  # end
+
+  def paint!(color)
+    @color = :blue
   end
-
-  
-
-
 end
