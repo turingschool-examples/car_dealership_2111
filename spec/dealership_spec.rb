4b9344930594da6b5557bc99de1ae247070cfdba
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 10:52:58 -0700

    test: can return the average price of a car pass

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -46,6 +46,7 @@ RSpec.describe Dealership do
     dealership.add_car(car_2)
     dealership.add_car(car_3)
     dealership.add_car(car_4)
+    # binding.pry
     expect(dealership.cars_by_make("Ford")).to eq[car_1]
     expect(dealership.cars_by_make("Toyota")).to eq[car_2,car_3]
     expect(dealership.cars_by_make("Chevrolet")).to eq[car_4]
@@ -62,5 +63,19 @@ RSpec.describe Dealership do
     dealership.add_car(car_3)
     dealership.add_car(car_4)
     expect(dealership.total_value).to eq(156000)
-    end
+  end
+
+  it 'can return the average price of a car' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.average_price_of_car).to eq(39000)
+  end
 end
