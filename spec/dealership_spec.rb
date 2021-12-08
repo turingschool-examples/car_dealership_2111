cb723c3438a534801b6e390e8d733035089578b9
Nick Cooley <cooley8706@gmail.com>
2021-12-08 10:26:33 -0700

    test: Pass inventory count = 0 test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Dealership do
   it 'has no inventory' do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
 
-    expect(dealership.inventory). to eq([])
+    expect(dealership.inventory).to eq([])
+    expect(dealership.inventory_count).to eq(0)
   end
 end
