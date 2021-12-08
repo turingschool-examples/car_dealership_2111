a28223d6bc182f8b4a6877b7c20aef60c6dcb3e7
Sam Iavri <sammi80134@gmail.com>
2021-12-08 09:17:00 -0700

    Car has a loan length and monthly payment and a total coast

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,14 @@
 class Car
   attr_reader :make, :model, :monthly_payment, :loan_length
 
-  def initialize(make_and_model, _monthly_payment, _loan_length)
+  def initialize(make_and_model, monthly_payment, loan_length)
     @make = make_and_model.split[0]
     @model = make_and_model.split[1]
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+
+  def total_cost
+    loan_length * monthly_payment
   end
 end
