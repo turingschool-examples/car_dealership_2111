5febe00724e36f5dd1f2fc723cf4e4991f645889
benistta <92293363+benistta@users.noreply.github.com>
2021-12-08 10:33:01 -0700

    Feat:added to inventory

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,14 +1,14 @@
 class Dealership
   attr_reader :name, :address, :inventory, :inventory_count
+
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
-
   end
 
   def inventory_count
-    @inventory.count 
+    @inventory.count
   end
 
   def add_car(car)
