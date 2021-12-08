037c5938cbf9dfc5392bf688ab70f86708b542dc
Tyler Ross <tross020890@gmail.com>
2021-12-08 10:11:16 -0600

    Feat: Add class Car

---
+++ lib/car.rb
@@ -0,0 +1,11 @@
+class Car
+
+  def initialize(name, monthly_payment, loan_length)
+    @name = name
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+    @make
+    @model
+  end
+
+end  
