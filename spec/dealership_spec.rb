13f390414dba0fc507914e788402c39e25a9591e
Jesse Ochs <jessescottochs@gmail.com>
2021-12-08 10:38:55 -0700

    Test: test 7 passes, details method returns correct hash

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -51,4 +51,17 @@ RSpec.describe Dealership do
     expect(dealership.cars_by_make("Toyota")).to eq(car_2)
     expect(dealership.cars_by_make("Ford")).to eq(car_1)
   end
+
+  it 'details returns hash' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
+  end
 end
