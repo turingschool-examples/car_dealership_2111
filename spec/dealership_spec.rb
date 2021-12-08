9f41b96398ba0e026afcb977c729da20098fcb28
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 09:56:41 -0700

    test:pass can add cars to the inventory pass

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -4,13 +4,26 @@ require './lib/dealership'
 
 RSpec.describe Dealership do
   it 'exists' do
-  dealership = Dealership.new("Acme Auto", "123 Main Street")
-  expect(dealership).to be_instance_of(Dealership)
-  end
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_instance_of(Dealership)
+    end
 
   it 'has inventory that can be counted' do
-  dealership = Dealership.new("Acme Auto", "123 Main Street")
-  expect(dealership.inventory).to eq([])
-  expect(dealership.inventory_count).to eq(0)
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+    expect(dealership.inventory_count).to eq(0)
+    end
+
+  it 'can add cars to the inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory).to eq([car_1, car_2])
+    expect(dealership.inventory_count).to eq(2)
   end
 end
