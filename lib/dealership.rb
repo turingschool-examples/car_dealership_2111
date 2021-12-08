0bfda515b730b3ee25b5bb199f3744be2bf6427d
Austin Moore <austinchristianmoore@gmail.com>
2021-12-08 12:16:27 -0500

    Feature: Pass It3 tests

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -2,17 +2,52 @@ class Dealership
   attr_reader :dealer_name,
               :address,
               :inventory,
-              :inventory_count
+              :inventory_count,
+              :value
 
   def initialize(dealer_name, address)
     @dealer_name      = dealer_name
     @address          = address
     @inventory        = []
     @inventory_count  = 0
+    @value            = 0
   end
 
   def add_car(car)
     @inventory << car
     @inventory_count += 1
   end
+
+  def has_inventory?
+    if @inventory_count = 0
+      return false
+    else
+      return true
+    end
+  end
+
+  def cars_by_make(make)
+    car_makes = []
+    @inventory.each do |car|
+      if car.make == make
+        car_makes << car
+      end
+    end
+    car_makes
+  end
+
+  def total_value
+    @inventory.each do |car|
+      @value += car.total_cost
+    end
+    @value
+  end
+
+  def details
+    detail = {
+      "total_value" => @value,
+      "address"     => @address
+    }
+  end
+
 end
