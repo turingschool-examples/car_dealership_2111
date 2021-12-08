290eebc9b03109642546eca6413481a36b97ce82
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-08 10:16:03 -0600

    Feat: add #paint! to Car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,5 @@
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
 
   def initialize(type, monthly_payment, loan_length)
     @type = type
@@ -7,12 +7,15 @@ class Car
     @model = @type.split(" ").last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = nil
   end
 
   def total_cost
     @monthly_payment * @loan_length
   end
 
-
+  def paint!(color)
+    @color = color
+  end
 
 end
