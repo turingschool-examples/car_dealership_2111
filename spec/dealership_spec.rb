9f3fe0c929d60e314a739b6b1fe6b403049603cb
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 12:09:46 -0500

    Test: create it sorts by price test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -101,4 +101,22 @@ RSpec.describe Dealership do
 
     expect(average_price_of_car).to eq("39,000")
   end
+
+  it 'sorts by price' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.cars_sorted_by_price.size).to eq(4)
+    expect(dealership.cars_sorted_by_price[0].model).to eq("Tercel")
+    expect(dealership.cars_sorted_by_price[1].model).to eq("Prius")
+    expect(dealership.cars_sorted_by_price[2].model).to eq("Bronco")
+    expect(dealership.cars_sorted_by_price[3].model).to eq("Mustang")
+  end
 end
