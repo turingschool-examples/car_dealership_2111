f61fdfa52d81166813e82b845d145de2daf22252
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-08 10:11:53 -0600

    Feat: add attr_reader and #total_cost to Car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,18 @@
 class Car
+  attr_reader :make, :model, :monthly_payment, :loan_length
 
   def initialize(type, monthly_payment, loan_length)
-    @type =
+    @type = type
+    @make = @type.split(" ").first
+    @model = @type.split(" ").last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
 
-  
+  def total_cost
+    @monthly_payment * @loan_length
+  end
+
+
 
 end
