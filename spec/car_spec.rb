19fb77a6cc8dda82552fe16a57e9da3f18e1f16b
Michael Brandt <msbrandt00@gmail.com>
2021-12-08 09:15:00 -0700

    Refactor: insert before hook, Test: paint method

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,39 +2,38 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
+  before(:each) do
+    @car = Car.new("Ford Mustang", 1500, 36)
+  end
   it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car).to be_a Car
+    expect(@car).to be_a Car
   end
 
   it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.make).to eq("Ford")
+    expect(@car.make).to eq("Ford")
   end
 
   it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.model).to eq("Mustang")
+    expect(@car.model).to eq("Mustang")
   end
 
   it 'has a monthly payment' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.monthly_payment).to eq(1500)
+    expect(@car.monthly_payment).to eq(1500)
   end
 
   it 'has a loan length' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.loan_length).to eq(36)
+    expect(@car.loan_length).to eq(36)
   end
 
   it 'has a total cost' do
-    car = Car.new("Ford Mustang", 1500, 36)
+    expect(@car.total_cost).to eq(54000)
+  end
 
-    expect(car.total_cost).to eq(54000)
+  it 'is initialized to not have a color' do
+    expect(@car.color).to be(nil)
+  end
+  it 'can be painted' do
+    @car.paint!(:blue)
+    expect(@car.color).to eq(:blue)
   end
 end
