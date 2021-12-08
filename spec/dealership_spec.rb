6f68fca2bed63c8186c17fb93f4046d9e3dc13f1
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-08 10:19:56 -0600

    Test: add spec file for Dealership

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it 'exists' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_a Dealership
+  end
