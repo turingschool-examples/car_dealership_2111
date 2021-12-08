bdfca31c80983f9cbe4a3c8e8de45c3895b91809
oscar santos <oscar303data@gmail.com>
2021-12-08 09:14:40 -0700

    "Methods and attributes for class car passed"
    Methods and attributes make,model, monthly_payment and loan_length
    passed successfully the tests.

--- lib/car.rb
+++ lib/car.rb
@@ -1,4 +1,13 @@
 class Car
+
+  attr_reader :make,
+              :model,
+              :monthly_payment,
+              :loan_length
   def initialize(car, monthly_payment, loan_length)
+    @make = car.split[0]
+    @model = car.split[1]
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
   end
 end
