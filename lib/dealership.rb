e307a384910b13647538d2862e2b0150c5ad1dd1
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 09:50:34 -0700

    feat: define inventory_count method and add inventory to attr_reader

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,6 +1,12 @@
 class Dealership
+  attr_reader :inventory
   def initialize(argument_1, argument_2)
     @argument_1 = argument_1
     @argument_2 = argument_2
+    @inventory = []
+  end
+
+  def inventory_count
+    @inventory.count
   end
 end
