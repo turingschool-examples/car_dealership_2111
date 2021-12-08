b9ae4ff61353331fc1bb2cf54e6984b4b4dca79f
Jimmy Page <jpage@knights.ucf.edu>
2021-12-08 09:47:36 -0700

    finished test for has inventory method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -36,5 +36,16 @@ describe Dealership do
   end
 
   it '#has_inventory?' do
-  end 
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.has_inventory?).to be(false)
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.has_inventory?).to be(true)
+  end
 end
