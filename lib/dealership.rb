9f3f5c098daa527800cd915d3a2092c58590eb01
Sam Smith <samlsmith424@gmail.com>
2021-12-08 08:38:47 -0800

    Feat: add inventory methods to take cars

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,9 +1,17 @@
 class Dealership
-  attr_reader :inventory, :inventory_count
+  attr_reader :inventory
   def initialize(name, location)
     @name = name
     @location = location
     @inventory = []
     @inventory_count = 0
+  end
+
+  def add_car(car)
+    @inventory << car
+  end
+
+  def inventory_count
+    @inventory.size
   end
 end
