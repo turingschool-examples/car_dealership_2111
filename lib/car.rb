48c11b8c24b6d8805e7d9366174fe6627224fed9
Tyler Ross <tross020890@gmail.com>
2021-12-08 10:27:04 -0600

    Feat: add color

--- lib/car.rb
+++ lib/car.rb
@@ -1,13 +1,15 @@
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
   def initialize(name, monthly_payment, loan_length)
     @name = name
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color
 
     split = name.split(' ')
     @make = split.first
     @model = split.last
+
   end
 
   def total_cost
