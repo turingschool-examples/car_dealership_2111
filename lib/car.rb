5401e71383754f0685679ea63060f4505f48d7e6
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-08 11:23:02 -0500

    feat: add color and paint! methods

--- lib/car.rb
+++ lib/car.rb
@@ -1,15 +1,19 @@
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
   def initialize(make_and_model, monthly_payment, loan_length)
     make_model = make_and_model.split(" ")
     @make = make_model.first
     @model = make_model.last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color
   end
 
   def total_cost
     @monthly_payment * @loan_length
   end
 
+  def paint!(color)
+    @color = color
+  end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -37,4 +37,16 @@ RSpec.describe Car do
 
     expect(car.total_cost).to eq(54000)
   end
+
+  it 'has can have a color' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.color).to eq(nil)
+  end
+
+  it 'can be blue' do
+    car = Car.new("Ford Mustang", 1500, 36)
+    car.paint!(:blue)
+    expect(car.color).to eq(:blue)
+  end
 end
