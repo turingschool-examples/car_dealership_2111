0246839ac63c7564f7256adba8985456fa4be4d1
Jesse Ochs <jessescottochs@gmail.com>
2021-12-08 09:38:09 -0700

    Feat: color method added to car class, returns nil

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,11 @@
 class Car
-  attr_reader :vehicle, :monthly_payment, :loan_length, :total_cost
+  attr_reader :vehicle, :monthly_payment, :loan_length, :total_cost, :color
   def initialize(vehicle, cost, months)
   @vehicle = vehicle
   @monthly_payment = cost
   @loan_length = months
   @total_cost = cost * months
+  @color = color
   end
 
   def make
