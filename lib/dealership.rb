dda4dc0708dd70b6d7f2561eb7a92edb7e5fece5
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-08 09:26:04 -0700

    feat: begin Dealership class

---
+++ lib/dealership.rb
@@ -0,0 +1,6 @@
+class Dealership
+  def initialize(name, address)
+    @name = name
+    @address = address
+  end
+end
---
+++ spec/dealership_spec.rb
@@ -0,0 +1,17 @@
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  before(:each) do
+    @dealership = Dealership.new("Acme Auto", "123 Main Street")
+    @car_1 = Car.new("Ford Mustang", 1500, 36)
+    @car_2 = Car.new("Toyota Prius", 1000, 48)
+    @car_3 = Car.new("Toyota Tercel", 500, 48)
+    @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+  end
+
+  it 'exists' do
+    expect(@dealership).to be_instance_of Dealership
+  end
+
+end
