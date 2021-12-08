d623673d23c4c6be1da58520af6c9810bb0f87ad
Austin Moore <austinchristianmoore@gmail.com>
2021-12-08 12:55:29 -0500

    Feature: Pseudocode It4 features. None complete

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -50,4 +50,37 @@ class Dealership
     }
   end
 
+  def average_price_of_car #refactor - how to get value from total_value
+    temp_value = 0
+    @inventory.each do |car| #this is bloated
+      temp_value += car.total_cost
+    end
+    average = temp_value / @inventory.length #needs to convert .to_s after inputting a comma
+    if average.to_s.chars.to_a.length >= 4 #if over 3, string needs commas
+      average.to_s.chars.to_a #create array
+      #combine every 3 characters
+      #input commas between groups of 3
+      #output whole string with commas in correct order
+    else #if not over 3 no commas necessary
+      average.to_s
+    end
+  end
+
+  def cars_sorted_by_price
+    #for each car
+    #return car.total_cost & save to array
+    #Does a sort method exist for an array? If not create comparisons & sort
+    #output
+  end
+
+  def inventory_hash
+    #create new empty hash
+    #for each car
+    #if no key exists for a car.make, then create a key with car.make
+    #if key exists, skip
+    #after each key is created, for each car
+    #If car.make == key, shovel car instance into array
+    #return hash
+  end
+
 end
