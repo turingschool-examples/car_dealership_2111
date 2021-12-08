1791c96e903e93cd24ebbe43d22ce8b5dbf69792
Michael Hicks <hicks.michael04@gmail.com>
2021-12-08 09:20:37 -0700

    Add def make method

---
+++ lib/car.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/car'
+
+class Car
+  attr_reader :make_and_model,
+              :monthly_payment,
+              :loan_length,
+              :color
+  def initialize(make_and_model, monthly_payment, loan_length)
+    @make_and_model = make_and_model
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+    @color = nil
+  end
+
+  def make
+    make_and_model_split = make_and_model.split()
+    make = make_and_model_split[0]
+
+  end
+
+
+
+end
