c4841cb9d9f25aeb6dd6fc2c9d7271d420eec456
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-08 11:49:42 -0500

    feat: add has_inventory? = true test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -46,4 +46,14 @@ RSpec.describe Dealership do
 
     expect(dealership.has_inventory?).to eq(false)
   end
+
+  it 'can count cars in inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.has_inventory?).to eq(true)
+  end
 end
