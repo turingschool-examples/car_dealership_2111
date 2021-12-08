aaacc4d9e6d423f3bffbcc5850b702df706d986f
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 12:13:38 -0500

    Test: create it gives inventory hash test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -119,4 +119,23 @@ RSpec.describe Dealership do
     expect(dealership.cars_sorted_by_price[2].model).to eq("Bronco")
     expect(dealership.cars_sorted_by_price[3].model).to eq("Mustang")
   end
+
+  it 'gives inventory hash' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.inventory_hash).to be_a(Hash)
+    expect(dealership.inventory_hash.keys).to eq(["Ford", "Toyota", "Chevrolet"])
+    expect(dealership.inventory_hash.values[0]).to be_a(Car)
+    expect(dealership.inventory_hash.values[1]).to be_a(Car)
+    expect(dealership.inventory_hash.values[2]).to be_a(Car)
+    expect(dealership.inventory_hash.values[3]).to be_a(Car)
+  end
 end
