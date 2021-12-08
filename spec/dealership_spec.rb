d3a0f2147edeaf70dbd495e7e8bbf21f66d76ca5
Sam Iavri <sammi80134@gmail.com>
2021-12-08 09:26:51 -0700

    Dealership exists

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,29 @@
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it 'exists' do
+    dealership = Dealership.new('Acme Auto', '123 Main Street')
+    expect(dealership).to be_a(Dealership)
+  end
+
+  xit 'has an inventory' do
+    dealership = Dealership.new('Acme Auto', '123 Main Street')
+    expect(dealership.inventory).to eq([])
+  end
+
+  xit 'has an inventory count' do
+    dealership = Dealership.new('Acme Auto', '123 Main Street')
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+  xit 'has an new inventory and count ' do
+    dealership = Dealership.new('Acme Auto', '123 Main Street')
+    car_1 = Car.new('Ford Mustang', 1500, 36)
+    car_2 = Car.new('Toyota Prius', 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    expect(dealership.inventory).to eq([car_1, car_2])
+    expect(dealership.inventory_count).to eq(2)
+  end
+end
