538e3bf9a50b77329279e455f18c3a3f3ef69e41
Jesse Ochs <jessescottochs@gmail.com>
2021-12-08 09:20:37 -0700

    Feat: Car class has make method that pulls the make from the vehicle argument

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,13 @@
 class Car
-
+  attr_reader :vehicle
   def initialize(vehicle, price, months)
   @vehicle = vehicle
   @price = price
   @months = months
   end
 
+  def make
+    @vehicle.split(" ").first
+
+  end
 end
