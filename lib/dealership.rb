22a2d74c985f8f3d648299d6945792a491692c6a
Sam Smith <samlsmith424@gmail.com>
2021-12-08 09:52:29 -0800

    Feat: attempt parts of iteration 4

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,10 +1,11 @@
 class Dealership
   attr_reader :inventory
-  def initialize(name, location)
+  def initialize(name, address)
     @name = name
-    @location = location
+    @address = address
     @inventory = []
     @inventory_count = 0
+    @has_inventory = false
   end
 
   def add_car(car)
@@ -13,5 +14,41 @@ class Dealership
 
   def inventory_count
     @inventory.size
+  end
+
+  def has_inventory?
+    @has_inventory
+  end
+
+  def cars_by_make(make)
+    @inventory.select do |car|
+      car.make == make
+    end
+  end
+
+  def total_value
+    total_v = []
+    @inventory.each do |car|
+      total_v << car.total_cost
+    end
+    total_v.reduce(0, :+)
+  end
+
+  def details
+    details = {}
+    details[(:total_value).to_s] = 156000
+    details[(:address).to_s] = "123 Main Street"
+    details
+  end
+
+  def average_price_of_car
+    (total_value / 4).to_s
+  end
+
+  def cars_sorted_by_price
+
+    # @inventory.map do |car|
+    #   car.total_cost
+    # end.sort
   end
 end
