7e8553eb53796cb7bf2ff01924ffe0150a6c6347
Sulaiman Birashk <sully.birashk@gmail.com>
2021-12-08 10:20:46 -0700

    Finish Iteration 2

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,6 +1,17 @@
 class Dealership
+  attr_reader :name, :addy, :inventory
   def initialize(name, addy)
     @name = name
     @addy = addy
+    @inventory = []
   end
+
+  def inventory_count
+    return @inventory.count
+  end
+
+  def add_car(car)
+    @inventory << car
+  end
+
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,11 +1,41 @@
 require './lib/car'
 require './lib/dealership'
 require 'pry'
+require 'rspec'
 
-RSpec.describe Car do
-  it 'exists' do
-    dealership = Dealership.new("Acme Auto", "123 Main Street"
-    expect(dealership).to be_a Dealership
+RSpec.describe Dealership do
+  it 'exist' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership).to be_instance_of(Dealership)
   end
-  
+
+  it 'has an inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.inventory).to eq([])
+  end
+
+  it "how many items are in inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+  it "adds cars to inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    #binding.pry
+    expect(dealership.inventory)
+  end
+
+  it 'counts inventory after adding cars' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    expect(dealership.inventory_count).to eq(2)
+  end
+
 end
