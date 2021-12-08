05a70b963f07b5bc87142c4039a3dd95459143f5
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-08 10:34:40 -0600

    Feat: add #add_car(car) to Dealership class. Test in spec file.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,11 +1,18 @@
 class Dealership
-  attr_reader :inventory, :inventory_count
+  attr_reader :inventory
 
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
-    @inventory_count = @inventory.count
+  end
+
+  def add_car(car)
+    @inventory << car
+  end
+
+  def inventory_count
+    @inventory.count
   end
 
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -14,7 +14,24 @@ RSpec.describe Dealership do
 
     expect(dealership.inventory).to eq([])
     expect(dealership.inventory_count).to eq(0)
-  end 
+  end
+
+  it "can add cars to inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory[0].model).to eq("Mustang")
+    expect(dealership.inventory[1].model).to eq("Prius")
+    expect(dealership.inventory_count).to eq(2)
+    expect(dealership.inventory.first).to be_instance_of(Car)
+  end
 
 
 end
