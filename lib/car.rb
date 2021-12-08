78f328284c8faa764f6e68978aa88b9cb290d3ee
Jesse Ochs <jessescottochs@gmail.com>
2021-12-08 09:30:44 -0700

    Feat: loan length method returns months argument

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,9 @@
 class Car
-  attr_reader :vehicle, :monthly_payment
+  attr_reader :vehicle, :monthly_payment, :loan_length
   def initialize(vehicle, cost, months)
   @vehicle = vehicle
   @monthly_payment = cost
-  @months = months
+  @loan_length = months
   end
 
   def make
