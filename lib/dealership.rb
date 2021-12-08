28854ed5a30f40ca5e70db9634dbe9746adc1a33
Joseph Galvin <jwgalvin@gmail.com>
2021-12-08 09:34:44 -0700

    Feat:Inventory and Count

---
+++ lib/dealership.rb
@@ -0,0 +1,14 @@
+class Dealership
+  attr_reader :name, :address, :inventory
+
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
+
+  def inventory_count
+    @inventory.count
+  end
+
+end
---
+++ spec/dealership_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it 'exists' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.inventory).to eq([])
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+end
