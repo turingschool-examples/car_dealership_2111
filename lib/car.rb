03ea43e423a7905f892967e073b48b7c3901dd46
Maximilian Wagner <maw3350@gmail.com>
2021-12-08 11:25:37 -0500

    feat: add car class

---
+++ lib/car.rb
@@ -0,0 +1,17 @@
+class Car
+  attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost
+  def initialize(make, model, monthly_payment, loan_length)
+    @make            = make
+    @model           = model
+    @monthly_payment = monthly_payment
+    @loan_length     = loan_length
+    # @total_cost      = []
+  end
+
+  # def total_cost(cost)
+  #   @total_cost      = []
+  #   # @total_cost.push(cost)
+  #   @total_cost << cost
+  # end
+  # @total_cost
+end
