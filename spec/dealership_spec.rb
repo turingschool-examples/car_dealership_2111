d64ee8a65ec07f4695e86a13c67ada547b64029a
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 11:19:34 -0500

    Test: create it starts with zero inventory test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -12,7 +12,12 @@ RSpec.describe Dealership do
   it 'has inventory' do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
 
-    expect(dealership.inventory).to eq([]) 
+    expect(dealership.inventory).to eq([])
   end
 
+  it 'starts with zero inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+  end
 end
