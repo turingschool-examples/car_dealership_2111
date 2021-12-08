2b13e12ebc5edb1d32a1af8da012d83326ef3461
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 09:50:55 -0700

    test: has inventory that can be counted pass

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -4,8 +4,13 @@ require './lib/dealership'
 
 RSpec.describe Dealership do
   it 'exists' do
-
   dealership = Dealership.new("Acme Auto", "123 Main Street")
   expect(dealership).to be_instance_of(Dealership)
-  end 
+  end
+
+  it 'has inventory that can be counted' do
+  dealership = Dealership.new("Acme Auto", "123 Main Street")
+  expect(dealership.inventory).to eq([])
+  expect(dealership.inventory_count).to eq(0)
+  end
 end
