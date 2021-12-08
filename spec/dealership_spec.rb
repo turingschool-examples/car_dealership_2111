9e6b2eaba22606705630773b0923c2d825204ec3
Jimmy Page <jpage@knights.ucf.edu>
2021-12-08 10:00:04 -0700

    wrote test for total value

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -62,4 +62,17 @@ describe Dealership do
     expect(dealership.cars_by_make("Toyota")).to eq([car_2, car_3])
     expect(dealership.cars_by_make("Ford")).to eq([car_1])
   end
+
+  it 'can tell you the total value of cars in the dealership' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.total_value).to eq(156000)
+  end
 end
