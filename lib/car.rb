590432720dbddd8347bbd9b4629b1dcc038ce774
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-08 09:13:06 -0700

    Test: Pass has monthly payment and loan length

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,10 @@
 class Car
+  attr_reader :make_model, :monthly_payment, :loan_length
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = make_model
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
+
+
 end
