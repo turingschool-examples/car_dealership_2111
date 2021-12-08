552ace554fdda1b10b431de5b72e67040bb9f7b3
Michael Brandt <msbrandt00@gmail.com>
2021-12-08 09:14:29 -0700

    Feature: add #paint method

--- lib/car.rb
+++ lib/car.rb
@@ -1,15 +1,20 @@
 class Car
-  attr_accessor :name, :monthly_payment, :loan_length, :make, :model
+  attr_accessor :name, :monthly_payment, :loan_length, :make, :model, :color
   def initialize(name, monthly_payment, loan_length)
     @name = name
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @make = @name.split(' ').first
     @model = @name.split(' ').last
+    @color = nil
   end
 
   def total_cost
     @monthly_payment * @loan_length
-  end 
+  end
+
+  def paint!(color)
+    @color = color.to_sym
+  end
 
 end
