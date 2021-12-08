3434650ec387921f709f239cc471ab9f1a41d810
Jimmy Page <jpage@knights.ucf.edu>
2021-12-08 09:26:55 -0700

    created initial test

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require './lib/car'
+require './lib/dealership'
+
+describe Dealership do
+  it 'exists' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_a(Dealership)
+  end
+end
