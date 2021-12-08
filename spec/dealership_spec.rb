c697b4fb944380b418aed192d7b0ba0a5ea74356
Jimmy Page <jpage@knights.ucf.edu>
2021-12-08 09:44:00 -0700

    added test for first iteration 3 methods

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -28,8 +28,13 @@ describe Dealership do
     car_1 = Car.new("Ford Mustang", 1500, 36)
     car_2 = Car.new("Toyota Prius", 1000, 48)
     dealership.add_car(car_1)
+    expect(dealership.inventory_count).to be(1)
+    expect(dealership.inventory).to eq([car_1])
     dealership.add_car(car_2)
-    expect(dealership.inventory).to be([car_1, car_2])
+    expect(dealership.inventory).to eq([car_1, car_2])
     expect(dealership.inventory_count).to be(2)
+  end
+
+  it '#has_inventory?' do
   end 
 end
