ddbe93810736827bd980f755fc295ee0de937a93
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 11:27:02 -0500

    Feat: create inventory_count attr

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,10 +1,11 @@
 require './lib/car'
 class Dealership
-  attr_reader :inventory
+  attr_reader :inventory, :inventory_count
 
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
+    @inventory_count = @inventory.size
   end
 end
