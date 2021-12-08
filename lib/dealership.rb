641e05b0f1c5325991544d1ff3011bfd69285f30
Michael Hicks <hicks.michael04@gmail.com>
2021-12-08 10:00:33 -0700

    Add #two new cars test

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,11 +4,21 @@ require './lib/dealership'
 require 'pry'
 
 class Dealership
-  attr_reader :name, :address, :inventory
+  attr_reader :name, :address, :inventory, :inventory_count
 
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
   end
+
+  def inventory_count
+    @inventory.count
+  end
+
+  def add_car(car)
+    @inventory.push car
+
+  end
+
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -15,8 +15,25 @@ RSpec.describe Dealership do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
 
     expect(dealership.inventory).to eq([])
-
   end
+
+  it "#inventory count" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+
+  it "#add two new cars" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    expect(dealership.inventory_count).to eq(2)
+  end
+
+
 
 
 end
