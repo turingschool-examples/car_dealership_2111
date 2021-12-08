db7e4a4922c3728c254ae913891830543f35c386
Jimmy Page <jpage@knights.ucf.edu>
2021-12-08 09:29:40 -0700

    wrote initialize tests for dealership spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -8,4 +8,16 @@ describe Dealership do
 
     expect(dealership).to be_a(Dealership)
   end
+
+  it 'has a name' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.name).to eq("Acme Auto")
+  end
+
+  it 'has an address' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.address).to eq("123 Main Street")
+  end
 end
