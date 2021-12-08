846018ecdef5b3d613bdfb503ec0586466d3f4a1
Austin Moore <austinchristianmoore@gmail.com>
2021-12-08 12:54:52 -0500

    Test: Create It4 tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -77,4 +77,46 @@ RSpec.describe Dealership do
     expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
   end
 
+  it 'returns average price of car' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.average_price_of_car).to eq("39,000")
+  end
+
+  it 'returns cars by price' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36) #54000
+    car_2 = Car.new("Toyota Prius", 1000, 48) #48000
+    car_3 = Car.new("Toyota Tercel", 500, 48) #24000
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24) #30000
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.cars_sorted_by_price).to eq([car_3, car_4, car_2, car_1])
+  end
+
+  it 'returns hash of cars by make' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36) #54000
+    car_2 = Car.new("Toyota Prius", 1000, 48) #48000
+    car_3 = Car.new("Toyota Tercel", 500, 48) #24000
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24) #30000
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.inventory_hash).to eq({"Ford" => [car_1], "Toyota" => [car_2, car_3], "Chevrolet" => [car_4]})
+  end
+
 end
