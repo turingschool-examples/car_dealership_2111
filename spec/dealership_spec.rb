19858d7fc82324d5397dd255f327525134ea41a0
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 10:23:28 -0700

    feat: define total_value method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe Dealership do
     expect(dealership.has_inventory?).to eq(false)
   end
 
-  xit '' do
+  xit 'Can return cars from inventory by make' do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
     car_1 = Car.new("Ford Mustang", 1500, 36)
     car_2 = Car.new("Toyota Prius", 1000, 48)
@@ -46,7 +46,21 @@ RSpec.describe Dealership do
     dealership.add_car(car_2)
     dealership.add_car(car_3)
     dealership.add_car(car_4)
+    expect(dealership.cars_by_make("Ford")).to eq[car_1]
+    expect(dealership.cars_by_make("Toyota")).to eq[car_2,car_3]
+    expect(dealership.cars_by_make("Chevrolet")).to eq[car_4]
   end
 
-
+  it 'can return the total value of cars in inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.total_value).to eq(156000)
+    end
 end
