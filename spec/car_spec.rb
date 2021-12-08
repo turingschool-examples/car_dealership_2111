0daa6c5c39ec5bcce2c90f5f43cc4dd286605129
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 11:10:38 -0500

    Test: create inital color of nil test

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -37,4 +37,10 @@ RSpec.describe Car do
 
     expect(car.total_cost).to eq(54000)
   end
+
+  it 'has an initial color of nil' do
+    car = Car.new("Ford Mustang", 1500, 36)
+
+    expect(car.color).to be(nil)
+  end
 end
