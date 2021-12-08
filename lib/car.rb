64edd48a0d1e46a6637c2016dbac6ff876eeb7c2
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 11:09:13 -0500

    Feat: create @total_cost attr

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,11 @@
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
+  attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost
 
   def initialize(car, monthly_payment, loan_length)
     @make = car.split(" ").first
     @model = car.split(" ").last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @total_cost = @monthly_payment * @loan_length
   end
 end
