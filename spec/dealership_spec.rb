6fb9f264d907bc2f05ce5fb444b45f6a7bc202dd
oscar santos <oscar303data@gmail.com>
2021-12-08 09:48:27 -0700

    Feat: Assertion of it has attributes block is created

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Dealership do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
     expect(dealership).to be_a(Dealership)
   end
+
+  it 'has attributes' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.name).to eq("Acme Auto")
+    expect(dealership.address).to eq("123 Main Street")
+    expect(dealership.inventory).to eq([])
+    expect(dealership.iventory_count).to eq(0)
+  end
 end
