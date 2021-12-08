b11be871b3700505d2f0879fc46ed6b58125f0b1
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-08 09:08:24 -0700

    Test: Pass Car exists

---
+++ lib/car.rb
@@ -0,0 +1,7 @@
+class Car
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = make_model
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+end
