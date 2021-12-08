dc266387ec056f363ebd42c874ee9e9b488cb12c
oscar santos <oscar303data@gmail.com>
2021-12-08 09:26:03 -0700

    Test: method total_cost is created and passed the test

--- lib/car.rb
+++ lib/car.rb
@@ -4,10 +4,15 @@ class Car
               :model,
               :monthly_payment,
               :loan_length
+
   def initialize(car, monthly_payment, loan_length)
     @make = car.split[0]
     @model = car.split[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+  end
+
+  def total_cost
+    @monthly_payment * @loan_length
   end
 end
